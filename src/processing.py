c54003660b608b5f0681f7b39ee703f55d0f565a
Anastasiya Zybchenko <smailenokzy@gmail.com>
2025-05-18 16:39:20 +0300

    add sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -10,3 +10,8 @@ operation_list = [
 def filter_by_state(operation_list: List[Dict[str, Any]], state: str = 'EXECUTED') -> list [Dict[str, Any]]:
     """Функция сортировки по ключу state"""
     return list((item for item in operation_list if item.get('state') == state))
+
+
+def sort_by_date(operation_list: List[Dict[str, Any]]) -> list[Dict[str, Any]]:
+    """Функция сортировки даты"""
+    return sorted(operation_list, key=lambda item: item['date'], reverse=True)
