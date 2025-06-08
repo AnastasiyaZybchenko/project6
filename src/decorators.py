12f2d6ee7cd3315657d880d1a732e6b373dc24d8
Anastasiya Zybchenko <smailenokzy@gmail.com>
2025-06-08 17:27:25 +0300

    add decorarors

---
+++ src/decorators.py
@@ -0,0 +1,35 @@
+from functools import wraps
+
+
+def log(filename = "logs.txt"):
+    """
+    декоратор для логирования функций используется для:
+    1) проверки входных данных
+    2) измерение времени работы декорируемой функции
+    """
+
+    def decor(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                result = func(*args, **kwargs)
+
+                if filename == None or filename == "":
+                    print(f"{func.__name__} ok")
+                else:
+                    with open("../logs/" + filename, "a") as file:
+                        file.write(f"{func.__name__} ok\n")
+            except Exception as e:
+                result = None
+
+                if filename == None or filename == "":
+                    print(f"{func.__name__} error: {e}. Inputs: {[*args]}{[*kwargs]}")
+                else:
+                    with open("../logs/" + filename, "a") as file:
+                        file.write(f"{func.__name__} error: {e}. Inputs: {[*args]}{[*kwargs]}\n")
+
+            return result
+
+        return wrapper
+
+    return decor
